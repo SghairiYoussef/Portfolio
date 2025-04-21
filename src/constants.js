cb7c1533e0d2f35ebc411fc5fb0073537f7884fe
Youssef Sghairi <sghairiyoussef10@gmail.com>
2025-04-21 17:04:14 +0100

    Update Resume

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ export const scaleFactor = 3;
 export const dialogueData ={
     "gitRepo": 'In this shelf, I keep all the projects I hardly work on on a daily basis, you can check all of my finished projects and the ones I am currently working on here in my <a href="https://github.com/sghairiyoussef" target="_blank">Github</a>!',
     "degreeFrame": "This is my degree frame.. You can tell it's empty, right? Well that's because I'm currently still a Software Engineering Student in the National Inst. of Applied Science and Technology (INSAT) in Tunisia! I'm currently in my 3rd year of study and I'm really enjoying it!",
-    "resumeDesk": 'This is my resume, you can check it out here! <a href="https://drive.google.com/file/d/1SK8mjBzJfVHt6JigB2cGdNvkx8QkjSlu/view?usp=sharing"> resume </a>! ',
+    "resumeDesk": 'This is my resume, you can check it out here! <a href="https://drive.google.com/file/d/19LQokawtq79L1ART-VMk6rNA-ohEajtk/view?usp=sharing"> resume </a>! ',
     "picFrame": 'This is a picture frame, you can see a picture of me here! <a href="https://www.linkedin.com/in/youssef-sghairi/"> LinkedIn </a>! ',
     "currentlyWorkingOn": "I'm currently working on a game called 'Echoes Of Frost', a 2D top-down action-adventure game that I'm developing using Godot Engine! You can contact me if needed for more information! I'm usually available with youssef.sghairi@insat.ucar.tn!",
     "Exit": "I hope you enjoyed your visit! If you have any questions or need more information, feel free to contact me at youssef.sghairi@insat.ucar.tn, To exit the room, you can close the tab!"
